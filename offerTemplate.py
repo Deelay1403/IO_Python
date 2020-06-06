fcc36b672d345c8e9bb87084ca1889d944e1d5b6
Patryk Szczodrowski <patrykszczodrowski1403@gmail.com>
2020-06-06 22:47:19 +0200

    logic works

---
+++ offerTemplate.py
@@ -0,0 +1,21 @@
+class OfferTemplate:
+    def __init__(self,
+    username='',
+    name='',
+    model='',
+    price='',
+    make='',
+    year='',
+    photo='',
+    typ='',
+    desc=''
+    ):
+        self.username = username
+        self.name = name
+        self.model = model
+        self.price = price
+        self.make = make
+        self.year = year
+        self.photo = photo
+        self.typ = typ
+        self.desc = desc
